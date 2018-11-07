da5f29f5034abde6f94413fbb0cdc9fbab74db86
Thomas Johnson <tjohnson1106@gmail.com>
2018-11-07 17:07:26 -0500

    added created post mutation

--- lib/graphical_web/schema.ex
+++ lib/graphical_web/schema.ex
@@ -16,4 +16,14 @@ defmodule GraphicalWeb.Schema do
       resolve(&Graphical.UserResolver.find/2)
     end
   end
+
+  mutation do
+    field :create_post, type: :post do
+      arg(:title, non_null(:string))
+      arg(:body, non_null(:string))
+      arg(:user_id, non_null(:integer))
+
+      resolve(&Graphical.PostResolver.create/2)
+    end
+  end
 end
